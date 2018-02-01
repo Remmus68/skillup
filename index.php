bb44fbffdab750c9517256977be85fadfae4f596
SkillUP Student <student@skillup.spb.ru>
2018-02-01 20:56:31 +0300

    First PHP lesson.

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-/opt/sources/index.php
+<?php
+
+$currentDate = time();
+$yesterday = $currentDate - 24 * 60 * 60;
+
+echo $currentDate . "<br />";
+echo date('d.m.Y H:i:s') . "<br />";
+echo date('d.m.Y H:i:s', $yesterday);
+
+require_once('template.php');
