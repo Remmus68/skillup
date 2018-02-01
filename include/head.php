87409c7cd46aeefc73439fcdb35d54538dd4eb4a
SkillUP Student <student@skillup.spb.ru>
2018-02-01 21:54:09 +0300

    First PHP lesson.

---
+++ include/head.php
@@ -0,0 +1,5 @@
+<head>
+    <title><?=PROJECT_NAME?></title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
+</head>
--- index.php
+++ index.php
@@ -1,10 +1,29 @@
 <?php
+    define('PROJECT_NAME', 'Имя проекта' );
+?>
+<!DOCTYPE html>
+<html>
+<?= require_once ('include/head.php');?>
+<body>
+<div class="header">
+    <h1>Заголовок</h1>
+</div>
+<div class="content">
+</div>
+
+<?php
+
+define('SECONDS_IN_DAY', 86400);
 
 $currentDate = time();
-$yesterday = $currentDate - 24 * 60 * 60;
+$yesterday = $currentDate - SECONDS_IN_DAY * 7;
 
 echo $currentDate . "<br />";
 echo date('d.m.Y H:i:s') . "<br />";
 echo date('d.m.Y H:i:s', $yesterday);
 
 require_once('template.php');
+?>
+
+</body>
+</html>
