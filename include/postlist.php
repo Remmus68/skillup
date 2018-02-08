fb2bf60ce42a029824c3d19251aaae7dbb2becc9
remmus68 <dsemenom@yandex.ru>
2018-02-08 03:23:17 +0300

    HomeWork Lesson#3

---
+++ include/postlist.php
@@ -0,0 +1,53 @@
+  <?php
+    $postList = [
+
+        [
+            'user' => [
+                'firstname' => 'Екатерина',
+                'lastname' => 'Захарова',
+                'userpic' => 'image/female-user.jpg',
+            ],
+            'date' => 'Добавлено 2 часа назад',
+            'foto' => 'image/post1.jpg',
+            'likes' => 125,
+            'description' => 'Теперь я тоже жду ебилдов.',
+            'taglist' => [
+                [
+                    'name' => 'gentoo',
+                    'link' => '#',
+                ],
+                [
+                    'name' => 'dwm',
+                    'link' => '#',  
+                ],
+            ],
+        ],
+
+        [
+            'user' => [
+                'firstname' => 'Алексей',
+                'lastname' => 'Черепанов',
+                'userpic' => 'image/male-user.jpg',
+            ],
+            'date' => 'Добавлено 3 часа назад',
+            'foto' => 'image/post2.jpg',
+            'likes' => 543,
+            'description' => 'Всех с пятницей!',
+            'taglist' => [
+                [
+                    'name' => 'пингвин',
+                    'link' => '#',
+                ],
+                [
+                    'name' => 'пиво',
+                    'link' => '#',  
+                ],
+                [
+                    'name' => 'пятница',
+                    'link' => '#',  
+                ],
+            ],
+        ],
+
+    ];
+?>
--- posts.php
+++ posts.php
@@ -1,62 +1,51 @@
 ﻿<?php
     define('PROJECT_NAME', 'Red eyes');
-    $pageTitle='Главная';
-?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title><?= $pageTitle . ' | ' .PROJECT_NAME ?></title>
-    <link rel="stylesheet" type="text/css" href="css/style.css" />
-</head>
-<body>
-    <div class="container">
-        <?php require_once('include/header.php'); ?>
-             <div class="content">
-                <div class="post">
-                    <div class="user_info">
-                            <img src="image/female-user.jpg" />
-                        <div class="name">Екатерина Захарова</div>
-                        <div class="posted">Добавлено 2 часа назад</div>
-                    </div>
-                    <div class="foto">
-                        <img src="image/post1.jpg">
-                    </div>
-                    <div class="post_info">
-                        <div class="like">
-                            <img src="image/heart.png">
-                            125
-                        </div>
-                        <div class="comment">
-                            Теперь я тоже жду ебилдов.
-                            <a href="#" class="hashtag">#gentoo</a>
-                            <a href="#" class="hashtag">#dwm</a>
-                        </div>
-                    </div>
-                </div>
+    $pageTitle = 'Главная';
 
+    require_once('include/postlist.php');
+
+    require_once('include/header.php');
+   //var_dump($postList);
+   //echo $postList[0]['user']['userpic'];
+   //foreach ($postList as $post) {
+     //  var_dump($post);
+      // echo '<br />';
+   //}
+    //var_dump($_SERVER);
+?>
+             <div class="content">
+
+                <?php
+                    foreach ($postList as $post) {
+                ?>
                 <div class="post">
                     <div class="user_info">
-                            <img src="image/male-user.jpg" />
-                        <div class="name">Алексей Черепанов</div>
-                        <div class="posted">Добавлено 3 часа назад</div>
+                        <img src=<?='"' . $post['user']['userpic'] . '"'?> />
+                        <div class="name"><?=$post['user']['firstname'] . ' ' . $post['user']['lastname']?></div>
+                        <div class="posted"><?=$post['date']?></div>
                     </div>
                     <div class="foto">
-                        <img src="image/post2.jpg">
+                        <img src=<?='"' . $post['foto'] . '"'?> />
                     </div>
                     <div class="post_info">
                         <div class="like">
                             <img src="image/heart.png">
-                            543
+                            <?=$post['likes']?>
                         </div>
                         <div class="comment">
-                            Всех с пятницей!
-                            <a href="#" class="hashtag">#пингвин</a>
-                            <a href="#" class="hashtag">#пиво</a>
-                            <a href="#" class="hashtag">#пятница</a>
+                            <?=$post['description']?>
+                            <?php
+                                if (isset($post['taglist'])) {
+                                    foreach ($post['taglist'] as $tag) {?>
+                            <a href="<?=$tag['link']?>" class="hashtag"><?='#' . $tag['name']?></a>
+                            <?php
+                                    }
+                                }
+                            ?>
                         </div>
                     </div>
                 </div>
+                <?php } ?>
+
             </div>
-        </div>
-    </body>
-</html>
+        <?php require_once('include/header.php'); ?>
